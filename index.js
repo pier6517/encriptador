30f52fbaa1d30e69d45da71f9d62fca61c729640
pier6517 <daniela.vallejo@est.iudigital.edu.co>
2023-05-25 19:08:46 -0500

    subiendo encriptador

---
+++ index.js
@@ -0,0 +1,53 @@
+const { default: Swal } = require("sweetalert2");
+
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let munieco = document.getElementById("imagen-principal");
+
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        munieco.src = "./img/Muñeco.png";
+    } else {
+        munieco.src = "./img/Muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingrese el texto que desea encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "marning");
+    }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let munieco = document.getElementById("imagen-principal");
+
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")  
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        munieco.src = "./img/Muñeco.png";
+    } else {
+        munieco.src = "./img/Muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingrese el texto que desea encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "marning");
+    }
+}
